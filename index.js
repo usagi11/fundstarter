16209355a448780a89bc2d0eccaf6595ee0647f9
Ubuntu <ubuntu@ip-172-31-6-194.ec2.internal>
2016-03-03 22:08:57 +0000

    minor changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-response.sendfile('fundstarter.html') 
+response.sendfile('Hello World') 
 })
 
 app.listen(app.get('port'), function() {
