e7904fddef6cafafee5fcccb8e23d83290014297
Ubuntu <ubuntu@ip-172-31-6-194.ec2.internal>
2016-03-13 15:29:47 +0000

    port changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 /* serves all the static files*/
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 /*serves main page*/
