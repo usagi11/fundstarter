9810a148740ba8d379ca20a4bb9465d8648ba309
usagi11 <asami.hatano@sjsu.edu>
2016-10-03 18:41:34 +0000

    make it better

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var fileName = 'public/index.html';
 var synchronous = fs.readFileSync(fileName,{root:__dirname});
 var buf = new Buffer(1024);
 
+//send a file by asynchronous
+
 http.createServer(function(req, res){                                                                   fs.readFile(fileName, {root:__dirname}, function(err, data){                                        if(err){                                                                                            res.writeHeader(200, {'Content-type': 'text/html'});                                                res.write("error");                                                                                 res.end();                                                                                          }                                                                                                   else{                                                                                               res.writeHeader(200);                                                                               res.write(data);                                                                                    res.end("Asynchronous read end");                                                                  }                                                                                                })
 }).listen(8080);                                                                                     
 
