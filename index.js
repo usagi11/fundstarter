278d6e80461381f4d92f6a1b3813aa273191406b
usagi11 <asami.hatano@sjsu.edu>
2016-10-03 07:27:31 +0000

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var http = require('http');
 var fs = require('fs');
 var port = Number(process.env.PORT || 8080);
 var fileName = 'public/index.html';
-var buf = new Buffer(1024);
 var synchronous = fs.readFileSync(fileName,{root:__dirname});
 
 
