c4a0cb2b4c23229b4f05b8bc642d513f88473aad
usagi11 <asami.hatano@sjsu.edu>
2016-10-03 21:16:05 +0000

    make it better

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 
 var http = require('http');
 var fs = require('fs');
-var port = Number(process.env.PORT || 8080);
 var fileName = 'public/index.html';
-var synchronous = fs.readFileSync(fileName,{root:__dirname});
 var buf = new Buffer(1024);
 
-//send by asynchronous
+//send using fs.open(), fs.stat(), fs.read()
 
 http.createServer(function(res,res){
-    fs.readFile(fileName, {root:__dirname}, function(err, file){
-	if(err){
-	    res.writeHeader(200, {'Content-type': 'text/html'});
-	    res.write("error");
-	    res.end();
-	}
-	else{
-	    res.writeHeader(200);
-	    res.write(file);
-	    res.end("Asynchronous read end");
-	}
-});
+    fs.exists(fileName, function(exists){
+	if(exists){
+	    fs.stat(fileName, function(error, stats){
+		fs.open(fileName, 'r' , function(err, fd){
+		    var buffer = new Buffer(stats.size);
+		        fs.read(fd, buf, 0, buf.length, 0, function(err, bytesRead, buffer){
+			    var data = buffer.toString('utf8', 0 , buffer.length);
+			    res.writeHeader(200, {'Content-type' : 'text/plain'});
+			    res.write("Read file by fs.read()\n");
+			    res.write(data);
+			    res.end();
+			    fs.close(fd);
+			});
+		    });
+		});
+	    }
+	});
 }).listen(8080)
 
 
